b6648ca94875271f2a102b2e7d86b09484612d8f
Cheick O Dembele <cdemb104@uottawa.ca>
2017-09-13 14:03:55 -0700

    mon premier commit

---
+++ game.js
@@ -0,0 +1,11 @@
+ Volume in drive C is Windows
+ Volume Serial Number is D472-B343
+
+ Directory of C:\projets\createbest-github
+
+2017-09-13  02:01 PM    <DIR>          .
+2017-09-13  02:01 PM    <DIR>          ..
+2017-09-13  02:01 PM                 0 game.js
+2017-09-13  01:53 PM                49 README.md
+               2 File(s)             49 bytes
+               2 Dir(s)  915,181,948,928 bytes free
